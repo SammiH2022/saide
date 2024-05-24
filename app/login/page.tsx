82679463195493a2059b00b2d0d5042559687ae3
SammiH2022 <96802164+SammiH2022@users.noreply.github.com>
2024-05-23 23:14:06 -0500

    add back session

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,15 +4,15 @@ import { redirect } from "next/navigation"
 import { GoogleSignInButton, GithubSignInButton } from "@/components/authButtons"
 
 export default async function LoginPage() {
-    // const session = await getServerSession(authConfig)
-    // console.log("Session: ", session)
+    const session = await getServerSession(authConfig)
+    console.log("Session: ", session)
 
-    // if (session) return redirect("/main")
+    if (session) return redirect("/main")
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <div className="flex flex-col items-center justify-center min-h-screen">
             <div className="flex flex-col items-center mt-10 p-10 shadow-md">
-                <h1>Sign In</h1>
+                <h1 className="text-lg font-bold">Sign In</h1>
                 <GoogleSignInButton />
                 <GithubSignInButton />
             </div>

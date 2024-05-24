2291b3792da8d0abea54ddab849079067509789d
SammiH2022 <96802164+SammiH2022@users.noreply.github.com>
2024-05-23 23:10:00 -0500

    add github sign in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,20 @@
 import getServerSession from "next-auth"
 import { authConfig } from "@/lib/auth"
 import { redirect } from "next/navigation"
-import GoogleSignInButton from "../../components/authButtons"
+import { GoogleSignInButton, GithubSignInButton } from "@/components/authButtons"
 
 export default async function LoginPage() {
-    const session = await getServerSession(authConfig)
-    console.log("Session: ", session)
+    // const session = await getServerSession(authConfig)
+    // console.log("Session: ", session)
 
-    if (session) return redirect("/main")
+    // if (session) return redirect("/main")
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <div className="flex flex-col items-center mt-10 p-10 shadow-md">
                 <h1>Sign In</h1>
                 <GoogleSignInButton />
+                <GithubSignInButton />
             </div>
         </div>
     )

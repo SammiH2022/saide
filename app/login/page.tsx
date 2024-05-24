e50ec623de97c71c6592a8c574907853b2c9c198
SammiH2022 <96802164+SammiH2022@users.noreply.github.com>
2024-05-23 23:26:17 -0500

    adjust login box background

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,7 +11,7 @@ export default async function LoginPage() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-            <div className="flex flex-col items-center mt-10 p-10 shadow-md">
+            <div className="flex flex-col items-center bg-stone-100 rounded-md p-10 pb-20 shadow-md">
                 <h1 className="text-lg font-bold">Sign In</h1>
                 <GoogleSignInButton />
                 <GithubSignInButton />
